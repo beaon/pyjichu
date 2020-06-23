61e0ea40e4a6d4213c6ea73b97b1390ccd2cb82e
fzl <1007303274@qq.com>
2020-06-23 17:33:22 +0800

    success

--- ex41.py
+++ ex41.py
@@ -2,46 +2,44 @@ import random
 from urllib.request import urlopen
 import sys
 
-
 WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
     "class %%%(%%%):":
         "Make a class named %%% that is-a %%%.",
-    "class %%%(object):\n\tdef  __init__(self, ***)":
-        "class %%% has-a __init__ that takes self and *** params.",
-    "class %%%(object): \n\tdef ***(self, @@@)":
-        "class %%% has-a function *** that takes self and @@@ params.",
+    "class %%%(object):\n\tdef __init__(self, ***)" :
+        "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+        "class %%% has-a function named *** that takes self and @@@ parameters.",
     "*** = %%%()":
         "Set *** to an instance of class %%%.",
-    "***,***(@@@)":
-        "From *** get the *** function, call it with params self, @@@.",
-    "***.*** = '***":
+    "***.***(@@@)":
+        "From *** get the *** function, and call it with parameters self, @@@.",
+    "***.*** = '***'":
         "From *** get the *** attribute and set it to '***'."
 }
 
-# do they want to dirll phrases first
+# do they want to drill phrases first
 if len(sys.argv) == 2 and sys.argv[1] == "english":
-    PHRASES_FIRST = True
+    PHRASE_FIRST = True
 else:
-    PHRASES_FIRST = False
+    PHRASE_FIRST = False
 
-
-# load up the words form the website
+# load up the words from the website
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(str(word.strip(), encoding="utf-8"))
-#print(WORDS)
 
 
 def convert(snippet, phrase):
-    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
-    other_name = random.sample(WORDS, snippet.count("***"))
+    class_names = [w.capitalize() for w in
+                   random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
-        param_count = random.randint(1, 3)
+        param_count = random.randint(1,3)
         param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
@@ -52,12 +50,14 @@ def convert(snippet, phrase):
             result = result.replace("%%%", word, 1)
 
         # fake other names
-        for word in other_name:
+        for word in other_names:
             result = result.replace("***", word, 1)
 
         # fake parameter lists
         for word in param_names:
             result = result.replace("@@@", word, 1)
+
+        results.append(result)
 
     return results
 
@@ -71,12 +71,12 @@ try:
         for snippet in snippets:
             phrase = PHRASES[snippet]
             question, answer = convert(snippet, phrase)
-            if PHRASES_FIRST:
+            if PHRASE_FIRST:
                 question, answer = answer, question
 
             print(question)
 
             input("> ")
-            print(f"ANSWER:  {answer}\n\n")
+            print("ANSWER:  %s\n\n" % answer)
 except EOFError:
     print("\nBye")
