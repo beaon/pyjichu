f76db3325f629ebd13e8dedc3e6e74d5dbca8662
fzl <1007303274@qq.com>
2020-06-29 15:53:44 +0800

    框架

---
+++ ex43.py
@@ -0,0 +1,54 @@
+class Scene():
+    #  没有继承的类可以不写括号，不然idle会报错
+    def enter(self):
+        pass
+
+
+class Engine:
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+
+class Death(Scene):
+    def enter(self):
+        pass
+
+
+class CentralCorridor(Scene):
+    def enter(self):
+        pass
+
+
+class LaserWeaponArmory(Scene):
+    def enter(self):
+        pass
+
+
+class TheBridge(Scene):
+    def enter(self):
+        pass
+
+
+class EscapePod(Scene):
+    def enter(self):
+        pass
+
+
+class Map:
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
+
